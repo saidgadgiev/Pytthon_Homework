f9c93889d2660244fc307f2614c2b33e3713af9e
Said Gadgiev <ackusha_dargo@bk.ru>
2023-02-23 23:02:06 +0300

    Apply suggestions from code review
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- Seminar_1/Task_8.py
+++ Seminar_1/Task_8.py
@@ -9,7 +9,7 @@
 
 n,m,k = int(input()),int(input()),int(input())
 
-if k < n * m and ((k % n == 0) or (k % m == 0)):
+if k < n * m and (k % n == 0 or k % m == 0):
     print(f'{n} {m} {k} -> YES')
 else:
     print(f'{n} {m} {k} -> NO')

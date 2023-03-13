962c243b5d5885d25169d6ed71a505be71d26019
Said Gadjiev <ackusha1984@ya.ru>
2023-03-13 23:09:42 +0300

    В одну строку

--- Seminar_7/Task_36.py
+++ Seminar_7/Task_36.py
@@ -14,13 +14,15 @@
 #                                               6 12 18 24 30 36
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    for i in range(1, num_rows + 1): # 
-        for j in range(1, num_columns + 1):
-            print(str(operation(i, j)).ljust(3), end = "")
-        print()
+
+    print (''.join([''.join ([f'{operation(i, j)}\t' for j in range(1, num_rows)])+'\n' for i in range(1, num_columns)]))
+
+    # for i in range(1, num_rows + 1): # 
+    #     for j in range(1, num_columns + 1):
+    #         print(str(operation(i, j)).ljust(3), end = "")
+    #     print()
     
 
 print_operation_table(lambda x, y: x * y)
-
 
 
